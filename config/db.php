ab6041aafcb126668a063eb460cdf7a5d4bca19f
simbo19888 <47590894+simbo19888@users.noreply.github.com>
2019-02-27 14:12:16 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,11 +1,6 @@
 <?php
 
-//return [
-//    'class' => 'yii\db\Connection',
-//    'dsn' => 'pgsql:host=192.168.100.102;dbname=postgres',
-//    'username' => 'postgres',
-//    'password' => 'qweasdzxc',
-//    'charset' => 'utf8',
+
 
 return [
     'class' => 'yii\db\Connection',
